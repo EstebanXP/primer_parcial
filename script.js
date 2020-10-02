d295cf9282d56b9d6e93535ac5e67276dfc39d59
Esteban Xelhuantzi <a324249@uach.mx>
2020-10-02 13:42:18 -0600

    antes de las pruebas con las funciones

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-function cambiarMesa(){
-    mesa1=document.getElementById('mtable');
+function cambiarMesa1(){
+    mesa1=document.getElementById('mtable1');
+    if(mesa1.src.match('mNoUso')){
+        mesa1.src="mUso.jpg";
+    }else{
+        mesa1.src="mNoUso.jpg";
+    }
+    iniciarTiempo();
+}
+function iniciarTiempo(){
+    alert("Hola mundo");
+}
+
+function cambiarMesa2(){
+    mesa1=document.getElementById('mtable2');
+    if(mesa1.src.match('mNoUso')){
+        mesa1.src="mUso.jpg";
+    }else{
+        mesa1.src="mNoUso.jpg";
+    }
+}
+function cambiarMesa3(){
+    mesa1=document.getElementById('mtable3');
+    if(mesa1.src.match('mNoUso')){
+        mesa1.src="mUso.jpg";
+    }else{
+        mesa1.src="mNoUso.jpg";
+    }
+}
+
+function cambiarMesa4(){
+    mesa1=document.getElementById('mtable4');
+    if(mesa1.src.match('mNoUso')){
+        mesa1.src="mUso.jpg";
+    }else{
+        mesa1.src="mNoUso.jpg";
+    }
+}
+
+function cambiarMesa5(){
+    mesa1=document.getElementById('mtable5');
+    if(mesa1.src.match('mNoUso')){
+        mesa1.src="mUso.jpg";
+    }else{
+        mesa1.src="mNoUso.jpg";
+    }
+}
+
+function cambiarMesa6(){
+    mesa1=document.getElementById('mtable6');
     if(mesa1.src.match('mNoUso')){
         mesa1.src="mUso.jpg";
     }else{
