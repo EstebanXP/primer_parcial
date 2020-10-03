95f0356912cf0187de402b6b31390f7bb70c7310
Esteban Xelhuantzi <a324249@uach.mx>
2020-10-03 13:13:00 -0600

    Funciona el contador, intento reiniciarlo

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function cambiarMesa(idmesaaux,idt1,idt2){
+    var inter1;
     mesa1=document.getElementById(idmesaaux);
     if(mesa1.src.match('mNoUso')){
         mesa1.src="mUso.jpg";
         iniciarTiempo(idt1);
-        moverReloj(idt2)
-        
+        inter1=moverReloj(idt2,mesa1);
     }else{
         mesa1.src="mNoUso.jpg";
+        mesa1.pausar(inter1);
     }
-    
-    
 }
 
 function iniciarTiempo(idt1){
@@ -22,25 +21,20 @@ function iniciarTiempo(idt1){
     
 }
 
-function moverReloj(idt2){
-    momento1=new Date()
-    hora=momento1.getHours()
-    minuto=momento1.getMinutes()
-    sgs=momento1.getSeconds()
-    imHora=hora+" : "+minuto+" : "+sgs
-    document.getElementById(idt2).innerHTML=imHora
-    setTimeout("moverReloj(idt2)",100)
-    moverReloj(idt2)
+function moverReloj(idt2,mesa){
+    
+      var n=0;
+      var l=document.getElementById(idt2);
+      var intervalo=setInterval(function(){
+          l.innerHTML = n;
+          n++;
+      },999);
+      
+      if(mesa.src.match('mUso')){
+          alert("Hola mundo");
+      }
 }
 
-
-
-function borrarReloj(id){
-    momento1=new Date()
-    hora=00;
-    minuto=00;
-    sgs=00;
-    imHora=hora+" : "+minuto+" : "+sgs
-    document.getElementById(id).innerHTML=imHora
+function pausar(inter){
+    clearInterval(inter);
 }
-
